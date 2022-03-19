163a8972b78a3317d986bfca99466cdc0b35f131
BeeMOre32 <windowssart01@gmail.com>
2022-03-19 13:55:48 +0900

    fix bug

--- js/count-water.js
+++ js/count-water.js
@@ -1,22 +1,23 @@
 const plusCounter = document.querySelector(".waterdrink-count__plus");
 const minusCounter = document.querySelector(".waterdrink-count__minus");
 const count = document.querySelector(".waterdrink__counter");
+const watercount = localStorage.setItem("water", parseInt("0"));
 
 function pluseCount() {
-  let watercount = parseInt(localStorage.getItem("water"));
-  watercount = watercount + 1;
-  count.innerText = `You drink water ${watercount}cup`;
-  localStorage.setItem("water", watercount);
-  if (watercount > 10) {
+  let watercounter = parseInt(localStorage.getItem("water"));
+  watercounter = watercounter + 1;
+  count.innerText = `You drink water ${watercounter}cup`;
+  localStorage.setItem("water", watercounter);
+  if (watercounter > 10) {
     count.innerText = "oh!! hey stop Drinking Water...";
   }
 }
 function minusCount() {
-  let watercount = parseInt(localStorage.getItem("water"));
-  watercount = watercount - 1;
-  count.innerText = `You drink water ${watercount}cup`;
-  localStorage.setItem("water", watercount);
-  if (watercount < 0) {
+  let watercounter = parseInt(localStorage.getItem("water"));
+  watercounter = watercounter - 1;
+  count.innerText = `You drink water ${watercounter}cup`;
+  localStorage.setItem("water", watercounter);
+  if (watercounter < 0) {
     count.innerText = "OOPS ERROR!";
   }
 }
